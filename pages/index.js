e34733f79fd2e77aee65f0d5dd47ad6775ba7d76
Julia G. Gomes <107192330+julia-gabgomes@users.noreply.github.com>
2026-06-29 21:36:40 +0000

    refactor: altera frase da página inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Um <strong>projeto de ponta-a-ponta</strong> que impacta alguém.</h1>;
+    return <h1>Um <strong>projeto de ponta-a-ponta</strong> que impacta alguém!</h1>;
 };
 
 export default Home;
